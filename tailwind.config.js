32734b8ab76ceceb45a5682dcc8afb6d4a64cdc4
Chris Worton <christian@boxapixels.com>
2025-03-27 19:07:34 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // Scans your HTML files
+    theme: {
+      extend: {}, // Add custom colors or sizes here if you want later
+    },
+    plugins: [],
+  }
